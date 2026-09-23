460fce738e38c66412b7d15e0420ab36e8685033
Fuscus95 <simon.vyncke@gmail.com>
2026-09-23 13:29:44 +0200

    Improve shared navigation accessibility

--- assets/js/components.js
+++ assets/js/components.js
@@ -28,7 +28,7 @@ const headerHTML = `
     >
       <a href="/">Home</a>
       <a href="/#excursies">Excursies</a>
-      <a href="/gidsen.html">Gidsen</a>
+      <a href="/gidsen">Gidsen</a>
       <a href="/#werkwijze">Werkwijze</a>
       <a href="/#over">Over ons</a>
     </nav>
